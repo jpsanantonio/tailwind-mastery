f3a31df7c49a214804f63495797e71ad629f50fa
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-10-15 21:00:13 -0700

    Styled Active Links

--- app/page.js
+++ app/page.js
@@ -3,9 +3,26 @@ import Message from "@/components/Message";
 export default function Home() {
   return (
     <>
-      {[...Array(40)].map(() => (
-        <Message />
-      ))}
+      <div className="bg-stone-800 w-60 flex flex-col">
+        <div className="px-3 h-12 flex items-center shadow-md">
+          Tailwind CSS
+        </div>
+        <div className="text-stone-300 p-3 overflow-y-scroll flex-1 space-y-2">
+          <p className="text-white">channel (unread)</p>
+          <p className="text-white">channel (unread)</p>
+          {[...Array(40)].map((_, i) => (
+            <p>channels {i}</p>
+          ))}
+        </div>
+      </div>
+      <div className="bg-stone-700 flex flex-col flex-1">
+        <div className="px-3 h-12 flex items-center shadow-md">General</div>
+        <div className=" flex-1 space-y overflow-y-scroll">
+          {[...Array(40)].map(() => (
+            <Message />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
